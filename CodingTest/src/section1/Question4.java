d78552c3396cb1f81a8c85f5454087d6ffce38f7
skarbgud1 <sjarbgud@gmail.com>
2021-04-21 00:26:41 +0900

    Section 1-4. 단어 뒤집기 (StringBuilder이용법 또는 인덱스를 통한 직접뒤집기)

---
+++ CodingTest/src/section1/Question4.java
@@ -0,0 +1,56 @@
+package section1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question4 {
+
+	public ArrayList<String> solution(int n, String[] str) {
+		ArrayList<String> answer = new ArrayList<>();
+
+		// case1. StringBuilder를 이용한 뒤집기 StringBuilder().reverse() 이용
+		for (String x : str) 
+		{
+			String tmp = new StringBuilder(x).reverse().toString();
+			answer.add(tmp);
+		}
+		
+		// case2. 처음 인덱스와 끝 인덱스를 tmp에 집어넣어서 교환하는 방식
+		
+		for (String x : str)
+		{
+			char[] s = x.toCharArray();
+			int lt = 0;
+			int rt = x.length() - 1;
+			while (lt < rt)
+			{
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = s[lt];
+				lt++;
+				rt--;
+			}
+			String tmp = String.valueOf(s);
+			answer.add(tmp);
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Question4 T = new Question4();
+		Scanner kb = new Scanner(System.in);
+
+		int n = kb.nextInt();
+		String str[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			str[i] = kb.next();
+		}
+
+		for (String x : T.solution(n, str)) {
+			System.out.println(x);
+		}
+
+		System.out.print(T.solution(n, str));
+	}
+}
