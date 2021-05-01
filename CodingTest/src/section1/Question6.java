c010da3715eae79ec7a262f30a0b0985bf108d46
skarbgud1 <sjarbgud@gmail.com>
2021-05-02 00:50:40 +0900

    Section 1-6. 중복 문자 제거

---
+++ CodingTest/src/section1/Question6.java
@@ -0,0 +1,31 @@
+package section1;
+
+import java.util.Scanner;
+
+public class Question6 {
+
+	public String solution(String str) {
+		String answer = "";
+
+		for (int i = 0; i < str.length(); i++) 
+		{
+			System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+			
+			if (str.indexOf(str.charAt(i)) == i) 
+			{
+				answer += str.charAt(i);
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Question6 T = new Question6();
+		Scanner kb = new Scanner(System.in);
+
+		String str = kb.next();
+
+		System.out.println(T.solution(str));
+	}
+}
