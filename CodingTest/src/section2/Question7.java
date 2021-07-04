da7e00f91d9038e247660eff002094fd74bf9564
skarbgud1 <sjarbgud@gmail.com>
2021-07-04 23:50:15 +0900

    Section 2-7. 점수 계산

---
+++ CodingTest/src/section2/Question7.java
@@ -0,0 +1,38 @@
+package section2;
+
+import java.util.Scanner;
+
+public class Question7 {
+	
+	public int solution(int n, int[] arr) {
+		int answer =0, cnt = 0;
+		for(int i = 0; i < n; i++)
+		{
+			if(arr[i] == 1)
+			{
+				cnt++;
+				answer += cnt;
+			}
+			else
+			{
+				cnt = 0;
+			}
+		}
+				
+	    return answer;
+	}
+
+	public static void main(String[] args) {
+		Question7 T = new Question7();
+		Scanner kb = new Scanner(System.in);
+
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++)
+		{
+			arr[i] = kb.nextInt();
+		}
+		System.out.println(T.solution(n,arr));	
+	}
+}
