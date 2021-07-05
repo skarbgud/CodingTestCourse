944c7f880a92715f0dba3ccf9fd9b4dbc346e909
skarbgud1 <sjarbgud@gmail.com>
2021-07-06 00:05:08 +0900

    Section 2-8. 등수구하기

--- CodingTest/src/section2/Question8.java
+++ CodingTest/src/section2/Question8.java
@@ -7,20 +7,19 @@ public class Question8 {
 	public int[] solution(int n, int[] arr) {
 		int [] answer = new int[n];
 		
-		for(int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)
 		{
 			int cnt = 1;
 			for (int j = 0; j < n; j++)
 			{
-				if(arr[j]> arr[i])
+				if (arr[i] < arr[j])
 				{
 					cnt++;
 				}
 			}
 			answer[i] = cnt;
 		}
-				
-	    return answer;
+		return answer;
 	}
 
 	public static void main(String[] args) {
