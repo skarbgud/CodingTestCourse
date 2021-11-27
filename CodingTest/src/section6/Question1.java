257303d22580d336b4f5c2279d698f1b2673d0fd
skarbgud <sjarbgud@gmail.com>
2021-11-27 23:44:57 +0900

    Section 6-1. 선택정렬

---
+++ CodingTest/src/section6/Question1.java
@@ -0,0 +1,43 @@
+package section6;
+
+import java.util.Scanner;
+
+public class Question1 {
+
+	public int[] solution(int n, int[] arr) {
+		
+		for (int i = 0; i < n-1; i++)
+		{
+			int idx = i;
+			for (int j = i+1; j < n; j++)
+			{
+				if (arr[j] < arr[idx])
+				{
+					idx = j;
+				}
+				int tmp = arr[i];
+				arr[i] = arr[idx];
+				arr[idx] = tmp;
+			}
+		}
+		
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		Question1 T = new Question1();
+		Scanner kb = new Scanner(System.in);
+
+		int n = kb.nextInt();
+		int [] arr = new int[n];
+		for (int i =0; i < n; i++)
+		{
+			arr[i] = kb.nextInt();
+		}
+
+		for (int x : T.solution(n, arr))
+		{
+			System.out.print(x + " ");
+		}
+	}
+}
