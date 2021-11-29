27fdb5a730541321a3afe66ca172260e8291497a
GyuHyeong Nam <sjarbgud@gmail.com>
2021-11-29 23:35:11 +0900

    Section 6-5. 중복확인(해시맵 => n)

--- CodingTest/src/section6/Question5.java
+++ CodingTest/src/section6/Question5.java
@@ -1,6 +1,7 @@
 package section6;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Question5 {
@@ -8,12 +9,20 @@ public class Question5 {
 	public String solution(int n, int[] arr) {
 		String answer = "U";
 		
-		Arrays.sort(arr);
-		
-		for (int i = 0; i < n-1; i++)
+//		Arrays.sort(arr);
+//		
+//		for (int i = 0; i < n-1; i++)
+//		{
+//			if (arr[i] == arr[i+1])
+//			{
+//				return "D";
+//			}
+//		}
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int x : arr)
 		{
-			if (arr[i] == arr[i+1])
-			{
+			map.put(x, map.getOrDefault(x, 0) + 1);
+			if(map.get(x) >= 2) {
 				return "D";
 			}
 		}
