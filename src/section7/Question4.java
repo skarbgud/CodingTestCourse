866a953ffbe79cd66390856c025273900381a59e
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-09 20:49:53 +0900

    Section 7-4. 피보나치 재귀(메모이제이션)

--- src/section7/Question4.java
+++ src/section7/Question4.java
@@ -5,18 +5,24 @@ public class Question4 {
 	static int[] fibo;
 	
 	public int DFS(int n) {
-		if (n == 1)
-			return fibo[n] = 1;
-		else if (n == 2)
-			return fibo[n] = 1;
+		// fibo에 이미 값이 저장이 되어있으니까 초기화된 0값이 아니면 배열에 있는 값 리턴
+		if (fibo[n] > 0) {
+			return fibo[n];
+		}
 		else {
-			return fibo[n] = DFS(n - 2) + DFS(n - 1);
+			if (n == 1)
+				return fibo[n] = 1;
+			else if (n == 2)
+				return fibo[n] = 1;
+			else {
+				return fibo[n] = DFS(n - 2) + DFS(n - 1);
+			}
 		}
 	}
 
 	public static void main(String[] args) {
 		Question4 T = new Question4();
-		int n = 10;
+		int n = 45;
 		// 인덱스를 위한 int 배열(n=1부터 시작이니까 +1)
 		fibo = new int[n+1];
 		
