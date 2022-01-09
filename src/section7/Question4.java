f07db75718d0c41f48a78cfe1219faa6d93834ff
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-09 20:44:36 +0900

    Section 7-4. 피보나치 재귀(배열에 저장)

--- src/section7/Question4.java
+++ src/section7/Question4.java
@@ -1,31 +1,29 @@
 package section7;
 
 public class Question4 {
-	/*
-		  		   D(5)
-		D(3)   		+   		D(4)
-	D(1) + D(2)				 D(2) + 	D(3)
-	 1    +  1 			      1     D(1) +  D(2)
-	 								1    +   1
-	 */
+	
+	static int[] fibo;
+	
 	public int DFS(int n) {
 		if (n == 1)
-			return 1;
+			return fibo[n] = 1;
 		else if (n == 2)
-			return 1;
+			return fibo[n] = 1;
 		else {
-			return DFS(n - 2) + DFS(n - 1);
+			return fibo[n] = DFS(n - 2) + DFS(n - 1);
 		}
 	}
 
 	public static void main(String[] args) {
 		Question4 T = new Question4();
-		int n = 5;
+		int n = 10;
+		// 인덱스를 위한 int 배열(n=1부터 시작이니까 +1)
+		fibo = new int[n+1];
+		
+		T.DFS(n);
 		
 		for (int i = 1; i <= n; i++) {
-			System.out.print(T.DFS(i) + " ");
+			System.out.print(fibo[i] + " ");
 		}
-		System.out.println("");
-		System.out.println(T.DFS(n));
 	}
 }
