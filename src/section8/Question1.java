15abd5d84d9c1ca49f7f2b926c9028e5fe002080
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-28 00:13:33 +0900

    Section 8-1. 합이 같은 부분집합(sum값이 total의 절반에 미치지 못할때 경우 추가)

--- src/section8/Question1.java
+++ src/section8/Question1.java
@@ -8,6 +8,7 @@ public class Question1 {
 	boolean flag = false;
 	public void DFS(int L, int sum, int[] arr) {
 		if (flag) return; // 나머지 스택에 있는 재귀들은 날려버리기
+		if (sum > total/2) return; // 합이 이미 /2보다 크면 비교할 필요도 없다
 		if (L == n) {
 			if ((total - sum) == sum) { // 절반이 되었을때 (or /2도 가능)
 				answer ="YES";
