152b009ae04726d23a504535a64ff30b3f9fb1d3
GyuHyeong Nam <sjarbgud@gmail.com>
2022-03-20 00:39:30 +0900

    Section 10-3. 최대부분증가수열(LIS)

---
+++ src/section10/Question3.java
@@ -0,0 +1,35 @@
+package section10;
+
+import java.util.Scanner;
+
+public class Question3 {
+	static int[] dy;
+
+	public int solution(int[] arr) {
+		int answer = 0;
+		dy = new int[arr.length];
+		dy[0] = 1;
+		for (int i = 1; i < arr.length; i++) {
+			int max = 0;
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[j] < arr[i] && dy[j] > max) {
+					max = dy[j];
+				}
+				dy[i] = max + 1;
+				answer = Math.max(answer, dy[i]);
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Question3 T = new Question3();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt(); // 계단 수
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		System.out.println(T.solution(arr));
+	}
+}
